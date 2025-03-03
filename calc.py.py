564ac407a39e6f8a9c662133fc0ccadcb5a00656
JAYASHRI D <its.djayashri@gmail.com>
2025-03-03 21:58:19 +0530

    Update calc.py.py

--- calc.py.py
+++ calc.py.py
@@ -1,15 +1,15 @@
-print("1 -Addition")
-print("2 -Subraction")
-print("3 -Multiplication")
-print("4 -Divide")
-option=int(input("choose an operation:"))
+print("1 - Addition")
+print("2 - Subraction")
+print("3 - Multiplication")
+print("4 - Divide")
+option = int(input("choose an operation: "))
 
 if(option in [1,2,3,4]):
-    num1 = int(input("Enter first number"))
-    num2 = int(input("Enter second number"))
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-    if(option== 1)
-    result = num1 + num2
+    if(option == 1):
+        result = num1 + num2
     elif(option == 2):
         result = num1 - num2
     elif(option == 3):
@@ -20,7 +20,4 @@ if(option in [1,2,3,4]):
 else:
     print("Invalid operation entered")
 
-    print("The result of the operation is{}".format(result))
-
-
-
+print("The result of the operation is {}".format(result))
